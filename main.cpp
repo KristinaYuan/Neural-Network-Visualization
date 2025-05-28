5a8e514d4e0d2ce6824e35cd39fd04e78989da26
KristinaYuan <Kristina_0418@163.com>
2025-05-28 21:23:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "CodeWings_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
     /*
     MainWindow w;
     w.show();
