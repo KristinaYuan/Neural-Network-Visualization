bbd8f3e4efa539ba52b201874df0b23a1f6155cc
KristinaYuan <Kristina_0418@163.com>
2025-05-28 21:04:15 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,10 @@
 #include "ui_mainwindow.h"
 #include "json_utils.h"
 #include "backend.h"
+#include "propertypanel.h"
 #include <QJsonDocument>
+#include <QIcon>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -61,3 +64,56 @@ void MainWindow::on_toolButton_clicked()
 
 }
 
+PropertyPanel* propertyPanel;
+
+void MainWindow::setupIconButton(QPushButton* button, const QString& iconPath, int size) {
+    button->setFixedSize(size, size);
+    button->setIcon(QIcon(iconPath));
+    button->setIconSize(QSize(size, size));
+    button->setStyleSheet("background-color:transparent");
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("CodeWings:Neural-Network-Visualization");
+
+    setupIconButton(ui->user, ":/Icon/user.png");
+    setupIconButton(ui->mode, ":/Icon/mode.png");
+    setupIconButton(ui->generate_code, ":/Icon/code.png");
+    setupIconButton(ui->generate_image, ":/Icon/image.png");
+    setupIconButton(ui->history, ":/Icon/history.png");
+    setupIconButton(ui->start_new, ":/Icon/new.png");
+    setupIconButton(ui->previous, ":/Icon/previous.png");
+    setupIconButton(ui->turnback, ":/Icon/turnback.png");
+    setupIconButton(ui->save, ":/Icon/save.png");
+}
+
+void MainWindow::on_user_clicked()
+{
+    qDebug() << "user 按钮点击了";
+    // 在这里实现你希望的功能逻辑
+}
+
+void MainWindow::on_mode_clicked()
+{
+    qDebug() << "mode 按钮点击了";
+
+}
+
+void MainWindow::on_generate_code_clicked()
+{
+    qDebug() << "代码生成中";
+}
+
+void MainWindow::on_generate_image_clicked()
+{
+    qDebug() << "神经网络图像生成中";
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
