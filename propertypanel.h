fb104e94ed8c788bc628821c09a0c9ff6109704e
KristinaYuan <Kristina_0418@163.com>
2025-05-28 21:00:23 +0800

    Create propertypanel.h

---
+++ propertypanel.h
@@ -0,0 +1,32 @@
+#ifndef PROPERTYPANEL_H
+#define PROPERTYPANEL_H
+
+#include <QWidget>
+#include <QFormLayout>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QMap>
+
+class PropertyPanel : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit PropertyPanel(QWidget *parent = nullptr);
+
+    void setLayerType(const QString& type);
+    void setParameters(const QMap<QString, QString>& params); // 设置参数字段和当前值
+
+signals:
+    void parametersUpdated(const QMap<QString, QString>& newParams); // 参数修改后发出信号
+
+private slots:
+    void onUpdateButtonClicked();
+
+private:
+    QFormLayout* layout;
+    QPushButton* updateBtn;
+    QMap<QString, QLineEdit*> fieldMap;
+    QString currentLayerType;
+};
+
+#endif // PROPERTYPANEL_H
