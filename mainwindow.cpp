963fedc5b5e5d486c002e1a6d5e4591d399374e7
KristinaYuan <Kristina_0418@163.com>
2025-05-28 21:17:18 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,66 +3,8 @@
 #include "json_utils.h"
 #include "backend.h"
 #include "propertypanel.h"
-#include <QJsonDocument>
 #include <QIcon>
 #include <QPushButton>
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    //connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::generateJson);//使用示例
-}
-void MainWindow::handleJsonData(const QString &jsonStr) {
-    QJsonDocument doc = QJsonDocument::fromJson(jsonStr.toUtf8());
-    if (doc.isObject()) {
-        QJsonObject obj = doc.object();
-        // 假设obj表示网络结构，从中提取层信息
-        QJsonArray layersArray = obj["layers"].toArray();
-        for (const QJsonValue &layerValue : layersArray) {
-            QJsonObject layerObj = layerValue.toObject();
-            NeuralLayer layer = NeuralLayer::fromJsonObject(layerObj);
-            // 后续可对layer进行操作
-        }
-    }
-}
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-/*void MainWindow::generateJson()//使用示例
-{
-    NeuralLayer layer1;
-    layer1.layerType = "Input";
-    layer1.neurons = 10;
-    layer1.activationFunction = "None";
-
-    NeuralLayer layer2;
-    layer2.layerType = "Hidden";
-    layer2.neurons = 20;
-    layer2.activationFunction = "ReLU";
-
-    NeuralLayer layer3;
-    layer2.layerType = "Output";
-    layer2.neurons = 5;
-    layer2.activationFunction = "ReLU";
-
-    QJsonArray layersArray;
-    layersArray.append(layer1.toJsonObject());
-    layersArray.append(layer2.toJsonObject());
-    layersArray.append(layer3.toJsonObject());
-
-    QJsonDocument doc(layersArray);
-    QString jsonStr = QString::fromUtf8(doc.toJson());
-    qDebug() << jsonStr;
-}*/
-
-
-void MainWindow::on_toolButton_clicked()
-{
-
-}
 
 PropertyPanel* propertyPanel;
 
@@ -78,6 +20,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::generateJson);//使用示例
+    
     setWindowTitle("CodeWings:Neural-Network-Visualization");
 
     setupIconButton(ui->user, ":/Icon/user.png");
@@ -113,7 +57,53 @@ void MainWindow::on_generate_image_clicked()
     qDebug() << "神经网络图像生成中";
 }
 
+void MainWindow::on_toolButton_clicked()
+{
+    
+}
+
+void MainWindow::handleJsonData(const QString &jsonStr) {
+    QJsonDocument doc = QJsonDocument::fromJson(jsonStr.toUtf8());
+    if (doc.isObject()) {
+        QJsonObject obj = doc.object();
+        // 假设obj表示网络结构，从中提取层信息
+        QJsonArray layersArray = obj["layers"].toArray();
+        for (const QJsonValue &layerValue : layersArray) {
+            QJsonObject layerObj = layerValue.toObject();
+            NeuralLayer layer = NeuralLayer::fromJsonObject(layerObj);
+            // 后续可对layer进行操作
+        }
+    }
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+/*void MainWindow::generateJson()//使用示例
+{
+    NeuralLayer layer1;
+    layer1.layerType = "Input";
+    layer1.neurons = 10;
+    layer1.activationFunction = "None";
+
+    NeuralLayer layer2;
+    layer2.layerType = "Hidden";
+    layer2.neurons = 20;
+    layer2.activationFunction = "ReLU";
+
+    NeuralLayer layer3;
+    layer2.layerType = "Output";
+    layer2.neurons = 5;
+    layer2.activationFunction = "ReLU";
+
+    QJsonArray layersArray;
+    layersArray.append(layer1.toJsonObject());
+    layersArray.append(layer2.toJsonObject());
+    layersArray.append(layer3.toJsonObject());
+
+    QJsonDocument doc(layersArray);
+    QString jsonStr = QString::fromUtf8(doc.toJson());
+    qDebug() << jsonStr;
+}*/
